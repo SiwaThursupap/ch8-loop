08e735026167e0cd8a9b1cd49a8ee3630c0622e7
SiwaThursupap <notsiwa1@gmail.com>
2026-03-13 16:50:21 +0700

    Work form 13-3-69

--- ForEach.java
+++ ForEach.java
@@ -1,12 +1,14 @@
 public class ForEach {
 
     public static void main(String[] args) {
-        double[] score = {85.5, 92.0, 78.5};
-        for (int i = 0; i< score.length; i++){
+        double[] score = { 85.5, 92.0, 78.5 };
+        for (int i = 0; i < score.length; i++) {
             IO.println(score[i]);
         }
 
-        for(int i = 0 ;i < score.length;i++);{
+        for (int i = 0; i < score.length; i++)
+            ;
+        {
             IO.println(score);
         }
 
